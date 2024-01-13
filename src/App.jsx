a5bf40115116d9ab9fe43207929accc31ec76aaa
ashraf jubara <username@example.com>
2024-01-13 14:10:41 +0200

    initial dependencies installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { NextUIProvider } from "@nextui-org/react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NextUIProvider>
+        <h1>Hello Next.js</h1>
+      </NextUIProvider>
+    </>
+  );
+}
+
+export default App;
